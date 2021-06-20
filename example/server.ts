5f08800fe8e0baf80e343110abb0aa9589b9dfb3
Alisue <lambdalisue@hashnote.net>
2021-06-21 07:02:41 +0900

    Rewrite example

--- example/server.ts
+++ example/server.ts
@@ -1,22 +1,23 @@
 import { Message, Session } from "../mod.ts";
+import { using } from "../deps_test.ts";
 
 // NOTE:
 // Do NOT use 'console.log()' while stdout is used to communicate with Vim
 
 console.warn("Session has connected");
-const server = new Session(Deno.stdin, Deno.stdout, (message: Message) => {
-  console.warn(`Unexpected message ${message} has received`);
-});
 
-server
-  .listen()
-  .then(() => console.warn("Client has disconnected"))
-  .catch((e) => console.error(e));
-
-console.warn(await server.redraw());
-console.warn(await server.ex("echo 'Hello'"));
-console.warn(await server.normal("vsp"));
-console.warn(await server.expr("v:version"));
-console.warn(await server.exprNoReply("v:version"));
-console.warn(await server.call("getcwd"));
-console.warn(await server.callNoReply("getcwd"));
+await using(
+  new Session(Deno.stdin, Deno.stdout, (message: Message) => {
+    console.warn(`Unexpected message ${message} has received`);
+  }),
+  async (server) => {
+    console.warn(await server.redraw());
+    console.warn(await server.ex("echo 'Hello'"));
+    console.warn(await server.normal("vsp"));
+    console.warn(await server.expr("v:version"));
+    console.warn(await server.exprNoReply("v:version"));
+    console.warn(await server.call("getcwd"));
+    console.warn(await server.callNoReply("getcwd"));
+  },
+);
+console.warn("Client has disconnected");
