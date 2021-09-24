84dc2da169f88afa4116ad5f51d408c0926eaec0
Alisue <lambdalisue@hashnote.net>
2021-09-24 21:54:21 +0900

    Upgrade dependencies

--- command_test.ts
+++ command_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.105.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.108.0/testing/asserts.ts";
 import * as command from "./command.ts";
 
 const isRedrawCommandTestCases = [
--- deps_test.ts
+++ deps_test.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.105.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.105.0/async/mod.ts";
-export { using } from "https://deno.land/x/disposable@v0.2.0/mod.ts";
+export * from "https://deno.land/std@0.108.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.108.0/async/mod.ts";
+export { using } from "https://deno.land/x/disposable@v1.0.0/mod.ts";
