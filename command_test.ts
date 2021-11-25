ea6ba34cc37c4eb1d0e384fed417ed63f3fe0e64
GitHub <noreply@github.com>
2021-11-25 01:06:11 +0000

    :package: Update deno dependencies

--- command_test.ts
+++ command_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.108.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 import * as command from "./command.ts";
 
 const isRedrawCommandTestCases = [
