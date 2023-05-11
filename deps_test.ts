e601d81fddd70497bd85dcf476d25819eca3476b
Alisue <lambdalisue@gmail.com>
2023-05-12 01:09:19 +0900

    :coffee: Remove `deps.ts` and `deps_test.ts`

--- deps_test.ts
+++
@@ -1,3 +0,0 @@
-export * from "https://deno.land/std@0.186.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.186.0/async/mod.ts";
-export { using } from "https://deno.land/x/disposable@v1.1.1/mod.ts";
--- session.ts
+++ session.ts
@@ -1,4 +1,13 @@
-import { Deferred, deferred, Disposable, JSONparser, streams } from "./deps.ts";
+import * as streams from "https://deno.land/std@0.186.0/streams/mod.ts";
+import {
+  Deferred,
+  deferred,
+} from "https://deno.land/std@0.186.0/async/deferred.ts";
+import type { Disposable } from "https://deno.land/x/disposable@v1.1.1/mod.ts";
+// NOTE:
+// streamparser-json must be v0.0.5 because it automatically end-up parsing without separator after v0.0.5
+// https://github.com/juanjoDiaz/streamparser-json/commit/577e918b90c19d6758b87d41bdb6c5571a2c012d
+import JSONParser from "https://deno.land/x/streamparser_json@v0.0.5/jsonparse.ts#=";
 import { isMessage, Message } from "./message.ts";
 import * as command from "./command.ts";
 import { Indexer } from "./indexer.ts";
@@ -84,7 +93,7 @@ export class Session implements Disposable {
   }
 
   private async listen(): Promise<void> {
-    const parser = new JSONparser();
+    const parser = new JSONParser();
     parser.onValue = (data, _key, _parent, stack) => {
       if (stack.length > 0) {
         return;
