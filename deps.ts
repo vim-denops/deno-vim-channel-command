25abbddc8ae2939113388b709a1e9a082c6c56c5
Alisue <lambdalisue@gmail.com>
2023-05-12 01:03:03 +0900

    :package: Do NOT use x/std

--- deps.ts
+++ deps.ts
@@ -1,9 +1,7 @@
 export * as io from "https://deno.land/std@0.186.0/io/mod.ts";
 export * as streams from "https://deno.land/std@0.186.0/streams/mod.ts";
-export { deferred } from "https://deno.land/x/std@0.186.0/async/deferred.ts";
-export type {
-  Deferred,
-} from "https://deno.land/x/std@0.186.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.186.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.186.0/async/deferred.ts";
 export type { Disposable } from "https://deno.land/x/disposable@v1.1.1/mod.ts";
 
 // NOTE:
