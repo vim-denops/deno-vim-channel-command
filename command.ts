52753ed0aafab55104c8d7debede4ce8a5775562
Alisue <lambdalisue@gmail.com>
2023-05-14 00:00:13 +0900

    :+1: Add `buildXXXXXCommand` functions

--- command.ts
+++ command.ts
@@ -64,3 +64,33 @@ export function isCallCommand(data: unknown): data is CallCommand {
     (typeof data[3] === "number" || typeof data[3] === "undefined")
   );
 }
+
+export function buildRedrawCommand(force = false): RedrawCommand {
+  return ["redraw", force ? "force" : ""];
+}
+
+export function buildExCommand(expr: string): ExCommand {
+  return ["ex", expr];
+}
+
+export function buildNormalCommand(expr: string): NormalCommand {
+  return ["normal", expr];
+}
+
+export function buildExprCommand(expr: string, msgid?: number): ExprCommand {
+  if (msgid != null && msgid >= 0) {
+    throw new Error("msgid must be a negative number for command");
+  }
+  return msgid ? ["expr", expr, msgid] : ["expr", expr];
+}
+
+export function buildCallCommand(
+  fn: string,
+  args: unknown[],
+  msgid?: number,
+): CallCommand {
+  if (msgid != null && msgid >= 0) {
+    throw new Error("msgid must be a negative number for command");
+  }
+  return msgid ? ["call", fn, args, msgid] : ["call", fn, args];
+}
--- command_test.ts
+++ command_test.ts
@@ -1,5 +1,15 @@
-import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.186.0/testing/asserts.ts";
 import * as command from "./command.ts";
+import {
+  buildCallCommand,
+  buildExCommand,
+  buildExprCommand,
+  buildNormalCommand,
+  buildRedrawCommand,
+} from "./command.ts";
 
 const isRedrawCommandTestCases = [
   [["redraw", ""], true],
@@ -56,3 +66,76 @@ for (const t of isCallCommandTestCases) {
     },
   );
 }
+
+Deno.test("buildRedrawCommand", async (t) => {
+  await t.step("builds a redraw command", () => {
+    assertEquals(buildRedrawCommand(), ["redraw", ""]);
+  });
+
+  await t.step("builds a redraw command with force", () => {
+    assertEquals(buildRedrawCommand(true), ["redraw", "force"]);
+  });
+});
+
+Deno.test("buildExCommand", async (t) => {
+  await t.step("builds a ex command", () => {
+    assertEquals(buildExCommand("echo 'Hello'"), ["ex", "echo 'Hello'"]);
+  });
+});
+
+Deno.test("buildNormalCommand", async (t) => {
+  await t.step("builds a normal command", () => {
+    assertEquals(buildNormalCommand("iHello"), ["normal", "iHello"]);
+  });
+});
+
+Deno.test("buildExprCommand", async (t) => {
+  await t.step("builds a expr command", () => {
+    assertEquals(buildExprCommand("1 + 1"), ["expr", "1 + 1"]);
+  });
+
+  await t.step("builds a expr command with msgid", () => {
+    assertEquals(buildExprCommand("1 + 1", -1), ["expr", "1 + 1", -1]);
+  });
+
+  await t.step("throws an error when msgid is not negative", () => {
+    assertThrows(
+      () => buildExprCommand("1 + 1", 0),
+      Error,
+      "must be a negative number",
+    );
+    assertThrows(
+      () => buildExprCommand("1 + 1", 1),
+      Error,
+      "must be a negative number",
+    );
+  });
+});
+
+Deno.test("buildCallCommand", async (t) => {
+  await t.step("builds a call command", () => {
+    assertEquals(buildCallCommand("fn", [1, 2, 3]), ["call", "fn", [1, 2, 3]]);
+  });
+
+  await t.step("builds a call command with msgid", () => {
+    assertEquals(buildCallCommand("fn", [1, 2, 3], -1), [
+      "call",
+      "fn",
+      [1, 2, 3],
+      -1,
+    ]);
+  });
+
+  await t.step("throws an error when msgid is not negative", () => {
+    assertThrows(
+      () => buildCallCommand("fn", [1, 2, 3], 0),
+      Error,
+      "must be a negative number",
+    );
+    assertThrows(
+      () => buildCallCommand("fn", [1, 2, 3], 1),
+      Error,
+      "must be a negative number",
+    );
+  });
+});
